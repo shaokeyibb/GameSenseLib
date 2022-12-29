e47c72963382deb8c809dfda67ef0a9d3afbb803
HikariLan <shaokeyibb@126.com>
2022-12-29 22:02:37 +0800

    Initial commit

---
+++ src/main/java/io/hikarilan/gamesenselib/GameSenseLib.java
@@ -0,0 +1,18 @@
+package io.hikarilan.gamesenselib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public final class GameSenseLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
